c0966299cca30eb355a4cd151f7119d083233afc
Hyungmo Gu <guhyungm7@gmail.com>
2023-02-28 17:51:35 -0700

    feat: Add progress #1

--- src/script.js
+++ src/script.js
@@ -1,91 +1,70 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import {
+    OrbitControls
+} from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
 
-/**
- * Base
- */
-// Debug
-const gui = new dat.GUI()
+class ImageToParticle {
+    constructor() {
+        this.gui;
+        this.canvas;
+        this.scene;
+        this.image;
+        this.init();
+    }
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
+    handleLoadImage() {
+        this.image = new Image();
+        this.image.src = "./assets/images/moradcreativeLogo.png";
+        this.image.onload = () => {
+            const ctx = this.canvas.getContext("2d");
 
-// Scene
-const scene = new THREE.Scene()
+            this.canvas.width = this.image.width;
+            this.canvas.height = this.image.width;
 
-/**
- * Test cube
- */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+            ctx.drawImage(this.image, 0, 0);
+            const data = ctx.getImageData(0, 0, this.image.width, this.image.height);
+            ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-/**
- * Sizes
- */
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+            const particles = [];
+            for (let y = 0, y2 = data.height; y < y2; y++) {
+                for (let x = 0, x2 = data.width; x < x2; x++) {
+                    if (data.data[(y * 4 * data.width) + (x * 4) + 3] > 128) {
+                        const particle = {
+                            x: x,
+                            y: y
+                        };
+                        particles.push(particle);
+                    }
+                }
+            }
+
+            ctx.fillStyle = "white";
+            for (let i = 0, j = particles.length; i < j; i++) {
+                const particle = particles[i];
+                ctx.fillRect(particle.x * 4, particle.y * 4, 2, 2);
+            }
+        };
+    }
+
+    handleLoadScene() {
+        this.scene = new THREE.Scene();
+    }
+
+    handleLoadCanvas() {
+        this.canvas = document.getElementById('scene');
+    }
+
+    handleLoadGUI() {
+        this.gui = new dat.GUI();
+    }
+
+    init() {
+        this.handleLoadGUI();
+        this.handleLoadCanvas();
+        this.handleLoadScene();
+        this.handleLoadImage();
+    }
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 3
-camera.position.y = 3
-camera.position.z = 3
-scene.add(camera)
-
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update controls
-    controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+new ImageToParticle();
